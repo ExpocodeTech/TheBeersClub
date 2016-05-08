4a5ce508fac2396daf500b5f95a515b18a29bb0e
Javier Exposito <javiexpo2003@gmail.com>
2016-05-08 22:12:20 +0200

    Clase 07

--- js/login.js
+++ js/login.js
@@ -1,57 +1,84 @@
-console.log('Login cargado');
-
-var loginData = {
-	user: null,
-	pass: null,
-	completed: 0
+var LOGIN_VERIFY = {
+	username: 'expocode',
+	password: '123'
 }
 
+var loginData = {
+	username: null,
+	password: null,
+	isValid: function(){
+		if (this.username != '' && this.password != '') {
+			if (LOGIN_VERIFY.username == this.username 
+				&& LOGIN_VERIFY.password == this.password) {
+				return true;
+			};
+		} else {
+			return false;
+		}
+	}
+}
+
+var signUpData = {
+	name: null,
+	lastname: null,
+	email: null,
+	password: null
+}
+
+function processForm(hasAccount) {
+	var loginMode = document.getElementById('loginMode');
+	if (hasAccount) {
+		var frmLogin = document.getElementById('frmLogin');
+		frmLogin.style.display = 'block';
+	} else {
+		var frmSignUp = document.getElementById('frmSignUp');
+		frmSignUp.style.display = 'block';
+	};
+	loginMode.style.display = 'none';
+}
+
+function processBack(){
+	console.log('processBack');
+	var loginMode = document.getElementById('loginMode');
+	
+	var frmLogin = document.getElementById('frmLogin');
+	var frmSignUp = document.getElementById('frmSignUp');
+
+	if (frmLogin.style.display == 'block') {
+		frmLogin.style.display = 'none';
+	} else if (frmSignUp.style.display == 'block') {
+		frmSignUp.style.display = 'none';
+	};
+	loginMode.style.display = 'block';
+}
 
 function processLogin(){
 	console.log('processLogin');
+	var frmLogin = document.getElementById('frmLogin');
 
-}
-
-function processNew(){
-	console.log('processNew');
-}
-
-function processSignUp(){
-	console.log('processSignUp');	
-}
-
-/*function processLogin (e) {
-	console.log(e);
-	e.preventDefault();
-
-	var loginData = {
-		user: null,
-		pass: null,
-		completed: 0
-	}
-
-	var elements = e.target.children;
-	for (var i = elements.length - 1; i >= 0; i--) {
-		var el = elements[i];
-
-		if (el.localName != 'input') {
-			continue;
-		};
-
-		if (el.name == 'username') {
-			loginData.user = el.value;
-			loginData.completed++;
-		} else	if (el.name == 'password') {
-			loginData.pass = el.value;
-			loginData.completed++;
+	if (frmLogin != null) {
+		loginData.username = frmLogin.children.username.value;
+		loginData.password = frmLogin.children.password.value;
+		if (loginData.isValid()) {
+			alert('Usuario logueado correctamente');
+			frmLogin.style.display = 'none';
+			displayAccountData();
+		} else {
+			alert('Error de logueo');
 		}
 	};
-
-	if (loginData.completed < 2) {
-		alert('Todos los campos son onbligatorios');
-	} else {
-
-	}
 }
-var frmLogin = document.getElementById('frmLogin');
-frmLogin.onsubmit = processLogin;*/
+
+function displayAccountData(){
+	var account = document.getElementById('account');
+	if (account.style.display == 'block') {
+		account.style.display = 'none'
+	} else {
+		account.style.display = 'block'
+	};
+}
+
+
+function processSignUp(){
+	console.log('processSignUp');
+}
